2db78943fcb99cac1e698dc9d3ad5f69fa794097
Ondrej Chaloupka <ochaloup@redhat.com>
2017-01-17 01:11:20 +0100

    next stage

---
+++ part1/01_simple/byteman-simple/src/main/java/org/jboss/qa/App.java
@@ -0,0 +1,14 @@
+package org.jboss.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String message = "Hello World!";
+        System.out.println( message );
+    }
+}
